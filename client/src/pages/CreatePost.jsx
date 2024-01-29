b416905d74506c054cbbf3fa1adfb5e5a3e77d86
Masterzerr <alejerry24@gmail.com>
2024-01-29 20:45:44 +0800

    complete create post

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,31 @@
 import { Button, FileInput, Select, TextInput } from 'flowbite-react'
-import React from 'react'
+import React, { useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import {getStorage} from 'firebase/storage';
+import { app } from '../firebase';
 
 export const CreatePost = () => {
+    const [file, setFile] = useState(null);
+    const [imageUploadProgress, setImageUploadProgress] = useState(null);
+    const [imageUploadError, setImageUploadError] = useState(null);
+    
+    const handleUploadImage = async () => {
+        try {
+            if(!file) {
+                return;
+            }
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + '-' + file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef.file);
+            uploadTask.on(
+
+            )
+        } catch(error) {
+            console.log(error)
+        }
+    }
   return (
     <div className='p-3 max-w-2xl mx-auto min-h-screen'>
         <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
@@ -27,12 +49,14 @@ export const CreatePost = () => {
                 <FileInput
                     type='file'
                     accept='image/*'
+                    onChange={(e) => setFile(e.target.files[0])}
                 />
                 <Button
                     type='button'
                     gradientDuoTone='purpleToBlue'
                     size='sm'
                     outline
+                    onClick={handleUploadImage}
                 >
                     Upload Image
                 </Button>
